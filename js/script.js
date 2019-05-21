6fd8eb613c85409d2642e5a6105cd90074f2141c
Monika Jung-Wrobel <monikajung84@gmail.com>
2019-05-21 10:15:58 +0200

    Add first variable

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var computerMove;
+var playerMove;
+playerMove = 'papier';
+computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+printMessage('Zagrałem ' + playerMove + '! Wygrywam!');
