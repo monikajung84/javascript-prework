80357b36d18f75d0cd43821be01b9e37c036b94c
Monika Jung-Wrobel <monikajung84@gmail.com>
2019-05-21 11:02:48 +0200

    Add displayed move

--- js/script.js
+++ js/script.js
@@ -4,3 +4,15 @@ playerMove = 'papier';
 computerMove = 'kamień';
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
 printMessage('Zagrałem ' + playerMove + '! Wygrywam!');
+
+var randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('Wylosowana liczba to:' + randomNumber);
+if (randomNumber == '1') {
+    computerMove = 'kamień';
+  } else if (randomNumber == '2') {
+    computerMove = 'papier';
+  } else {
+    computerMove = 'nożyce';
+  }
+  printMessage('Mój ruch: ' + computerMove);
