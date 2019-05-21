cfcdd394d1c96efd7f48249f2800f64fdf822a94
Monika Jung-Wrobel <monikajung84@gmail.com>
2019-05-21 12:06:41 +0200

    Add displayed move player

--- js/script.js
+++ js/script.js
@@ -1,11 +1,5 @@
-var computerMove;
-var playerMove;
-playerMove = 'papier';
-computerMove = 'kamień';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-printMessage('Zagrałem ' + playerMove + '! Wygrywam!');
 
-var randomNumber;
+var computerMove, randomNumber;
 randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('Wylosowana liczba to:' + randomNumber);
 if (randomNumber == '1') {
@@ -15,4 +9,18 @@ if (randomNumber == '1') {
   } else {
     computerMove = 'nożyce';
   }
-  printMessage('Mój ruch: ' + computerMove);
+  printMessage('Computer ruch: ' + computerMove);
+
+  var playerMove, playerInput;
+  playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+  console.log('Wpisana liczba to:' + playerInput);
+  if (playerInput == '1') {
+    playerMove = 'kamień';
+  } else if (playerInput == '2') {
+    playerMove = 'papier';
+  } else if (playerInput == '3') {
+    playerMove = 'nożyce';
+  } else {
+    playerMove = 'kamień';
+  }
+  printMessage('Twój ruch: ' + playerMove);
