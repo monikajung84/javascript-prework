2971861322e8cd07b02ec54617daf837fc7e07c2
Monika Jung-Wrobel <monikajung84@gmail.com>
2019-05-29 10:36:21 +0200

    Add variable const

--- js/script.js
+++ js/script.js
@@ -43,9 +43,9 @@ function buttonClicked(argButtonName) {
   }
 }
 
-buttonRock = document.getElementById('button-rock');
-buttonPaper = document.getElementById('button-paper');
-buttonScissors = document.getElementById('button-scissors');
+const buttonRock = document.getElementById('button-rock');
+const buttonPaper = document.getElementById('button-paper');
+const buttonScissors = document.getElementById('button-scissors');
 buttonRock.addEventListener('click', function() { buttonClicked('rock'); });
 buttonPaper.addEventListener('click', function() { buttonClicked('paper'); });
 buttonScissors.addEventListener('click', function() { buttonClicked('scissors'); });
