03678cc435d0f199cceed2a2023b31ff64b74916
Monika Jung-Wrobel <monikajung84@gmail.com>
2019-05-29 17:13:52 +0200

    Add swich

--- js/script.js
+++ js/script.js
@@ -1,51 +1,59 @@
+const buttonRock = document.getElementById("button-rock");
+const buttonPaper = document.getElementById("button-paper");
+const buttonScissors = document.getElementById("button-scissors");
 
-function buttonClicked(argButtonName) {
-  clearMessages();
-  console.log(argButtonName + ' Was clicked');
-  const playerMove = argButtonName;
-  console.log('player move is: ' + playerMove);
-  const randomNumber = Math.floor(Math.random() * 3 + 1);
-  console.log('Draw number is: ' + randomNumber);
-  const computerMove = getMoveName(randomNumber);
-  console.log('Computer move is: ' + computerMove);
-  displayResult(playerMove, computerMove);
+function getComputerMove(){
+  const moves = ['rock', 'paper', 'scissors'];
+  const randomNumber = Math.floor(Math.random() *3+1);
+  return moves [randomNumber];
+}
 
+function win(){
+  printMessage('Win!');
+}
+function lose(){
+  printMessage('Lose!');
+}
+function draw(){
+  printMessage('Draw!');
+}
 
-  function getMoveName(argMoveId) {
-    console.log('the getMoveName function with the argument was called: ' + argMoveId);
-    if (argMoveId == 1) {
-      return 'rock';
-    } else if (argMoveId == 2) {
-      return 'paper';
-    } else if (argMoveId == 3) {
-      return 'scissors';
-    } else {
-      printMessage('I do not know move about id ' + argMoveId + '. I assume that it was about "rock".');
-      return 'rock';
-    }
-  }
-  printMessage('Your move: ' + playerMove);
-  
-  function displayResult(argPlayerMove, argComputerMove) {
-    console.log('the displayResults function with the argument was called: ' + argPlayerMove + ', ' + argComputerMove);
-    if (argPlayerMove == 'paper' && argComputerMove == 'rock') {
-      printMessage('Win!');
-    } else if (argPlayerMove == 'rock' && argComputerMove == 'scissors') {
-        printMessage('Win!');
-    } else if (argPlayerMove == 'scissors' && argComputerMove == 'paper') {
-        printMessage('Win!');
-    } else if (argPlayerMove == argComputerMove) {
-        printMessage('Draw');
-    } else {
-      printMessage('Lose :(');
-    }
-    printMessage('I have chosen: ' + argComputerMove + ', and You have chosen: ' + argPlayerMove);
+function game(playerMove){
+  const computerMove = getComputerMove();
+  console.log("ruch gracza " + playerMove);
+  console.log("ruch compa " + computerMove);
+  switch (playerMove + computerMove){
+    case "paperrock":
+    case "scissorsrock":
+    case "scissorspaper":
+      win();
+      break;
+    case "rockpaper":
+    case "paperscissors":
+    case "rockscissors":
+      lose();
+      break;
+    case "paperpaper":
+    case "scissorsscissors":
+    case "rockrock":
+      draw();
+      break;
   }
 }
 
-const buttonRock = document.getElementById('button-rock');
-const buttonPaper = document.getElementById('button-paper');
-const buttonScissors = document.getElementById('button-scissors');
-buttonRock.addEventListener('click', function() { buttonClicked('rock'); });
-buttonPaper.addEventListener('click', function() { buttonClicked('paper'); });
-buttonScissors.addEventListener('click', function() { buttonClicked('scissors'); });
+function main(){
+  buttonRock.addEventListener('click',function(){
+    console.log("kliknąłeś kamien");
+    game ("rock");
+  })
+  buttonPaper.addEventListener('click',function(){
+    console.log("kliknąłeś papier");
+    game ("paper");
+  })
+  buttonScissors.addEventListener('click',function(){
+    console.log("kliknąłeś nożyce");
+    game ("scissors");
+  })
+}
+
+main();
